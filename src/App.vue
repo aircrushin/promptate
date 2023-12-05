f1f9833b153dce88a3a3c628af798f90d8adb833
UltraV <1216564567@qq.com>
2023-12-05 18:38:46 +0800

    change layout

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,49 @@
 <template>
   <div class="app">
     <div class="left-container">
-      <textarea v-model="text" class="text"/>
+      <textarea v-model="text" class="text" />
     </div>
     <div class="right-container">
-      <Navigator @navigate="handleNavigation" />
+      <Navigator @navigate="handleNavigation" :buttons="buttons"/>
       <div class="card-container">
-        <Card 
-          v-for="(card, index) in filteredCards" 
-          :key="index" 
-          :text="card" 
-          @click="addText(card)" 
-        />
+        <Card v-for="item in contentItems" :key="item.button" :text="item.text" v-show="activeButton === item.button" @click="addText(item.text)"></Card>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';  
+import { ref } from 'vue';
 import Card from './components/Card.vue';
 import PromptWork from './components/PromptWork.vue'
 import Navigator from './components/Navigator.vue';
 
 // 使用 ref 来创建响应式数据
 const text = ref('');
-const cards = ref(['卡片1内容1', '卡片2内容2']); // 卡片内容
+const buttons = ["内容1", "内容2", "内容3", "内容4", "内容5", "内容6"];
+const contentItems = [
+  { button: '内容1', text: '自定义内容1' },
+  { button: '内容2', text: '自定义内容2' },
+  { button: '内容3', text: '自定义内容3' },
+  { button: '内容4', text: '自定义内容4' },
+  { button: '内容5', text: '自定义内容5' },
+  { button: '内容6', text: '自定义内容6' },
+  // ... 更多内容
+];
+const activeButton = ref('');
+
 
 // 方法
+const handleNavigation = (button: string) => {
+  activeButton.value = button;
+};
+
 const addText = (cardText: string) => {
   text.value += cardText;
   console.log(text);
   //console.log(111);
 };
 
-const currentNav = ref('内容1'); // 当前选中的导航
-
-const filteredCards = computed(() => {
-  // 根据当前选中的导航过滤卡片
-  return cards.value.filter(card => card.includes(currentNav.value));
-});
-
-const handleNavigation = (navItem: string) => {
-  currentNav.value = navItem;
-};
 
 </script>
 
@@ -54,11 +54,13 @@ const handleNavigation = (navItem: string) => {
 }
 
 .left-container {
-  width: 20%; /* 或根据需要调整 */
+  width: 20%;
+  /* 或根据需要调整 */
 }
 
 .right-container {
-  width: 30%; /* 或根据需要调整 */
+  width: 30%;
+  /* 或根据需要调整 */
 }
 
 .card-container {
@@ -70,5 +72,6 @@ const handleNavigation = (navItem: string) => {
 .text {
   height: 300px;
 }
+
 /* 其他组件样式 */
 </style>
